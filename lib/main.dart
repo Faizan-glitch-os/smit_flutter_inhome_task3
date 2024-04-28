a92f02a2f5516cf603d29fbdf9c9b99741b89913
Faizan Ahmad <faizanahmad2582001@gmail.com>
2024-04-28 06:42:22 +0500

    Task3 completed and optimized

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
+  runApp(const MaterialApp(
     home: Task3(),
   ));
 }
@@ -14,51 +14,51 @@ class Task3 extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.white60,
       appBar: AppBar(
-        title: Center(child: Text('Task3')),
+        title: const Center(child: Text('Task3')),
       ),
       body: Center(
         child: Container(
-          margin: EdgeInsets.only(top: 100, bottom: 100),
-          padding: EdgeInsets.only(left: 50, right: 50),
+          margin: const EdgeInsets.only(top: 100, bottom: 100),
+          padding: const EdgeInsets.only(left: 50, right: 50),
           decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(10), color: Colors.white),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Container(
-                decoration:
-                    BoxDecoration(shape: BoxShape.circle, color: Colors.green),
-                child: Icon(
+                decoration: const BoxDecoration(
+                    shape: BoxShape.circle, color: Colors.green),
+                child: const Icon(
                   Icons.check_rounded,
                   color: Colors.white,
                   size: 150,
                 ),
               ),
-              SizedBox(
+              const SizedBox(
                 height: 40,
               ),
-              Text(
+              const Text(
                 'Congratulations!',
                 style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
               ),
-              SizedBox(
+              const SizedBox(
                 height: 20,
               ),
-              Text(
+              const Text(
                 'Your account is ready to use.',
                 style: TextStyle(fontSize: 20),
               ),
-              SizedBox(
+              const SizedBox(
                 height: 30,
               ),
               Container(
-                padding:
-                    EdgeInsets.only(left: 80, right: 80, top: 10, bottom: 10),
+                padding: const EdgeInsets.only(
+                    left: 80, right: 80, top: 10, bottom: 10),
                 decoration: BoxDecoration(
                   color: Colors.orangeAccent,
                   borderRadius: BorderRadius.circular(50),
                 ),
-                child: Text(
+                child: const Text(
                   'Go to Home',
                   style: TextStyle(
                     color: Colors.white,
